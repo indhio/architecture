98ef7ba92d946766e875a43bb645d927608d8450
indhio <indhio@gmail.com>
2017-02-15 21:33:47 -0200

    Change-Id: I728b3edc6c7f24246ad4b78d5b4e2c41c85b36f8 Signed-off-by: indhio <indhio@gmail.com>

--- it-architecture-core/src/main/java/com/indhio/architecture/crypt/CryptMD5.java
+++ it-architecture-core/src/main/java/com/indhio/architecture/crypt/CryptMD5.java
@@ -14,7 +14,7 @@ public class CryptMD5 implements Serializable {
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
 			byte[] array = md.digest(md5.getBytes());
-			StringBuffer sb = new StringBuffer();
+			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < array.length; ++i) {
 				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
 			}
